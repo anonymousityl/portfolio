d385bfe98253817870013b6574d21b0f720594bb
anonymousityl <81506470+anonymousityl@users.noreply.github.com>
2024-01-16 02:59:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height =sec.offsecHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id +']')
+                    .classList.add('active')    
+                })
+        }
+        
+    });
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
